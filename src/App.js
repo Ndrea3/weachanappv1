29bdc199a8d8ef46c666c6ffdf44c84e02357da0
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-06-28 09:52:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-
-import './App.css';
+import "./App.css";
 import background from "./images/coffee-and-donuts-pop-color-background.png";
-
 
 function WebApp() {
   return (
-    <div className="Webpage" style={{ backgroundImage: `url(${background})`,
-      height: "100vh",
-      marginTop: "-70px",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat"}}>
-  
+    <div
+      className="Webpage"
+      style={{
+        backgroundImage: `url(${background})`,
+        height: "100vh",
+        marginTop: "-70px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <h1 className="Header">
-      The Weather Be Like...
-        <span role="img" aria-label="Thinking Face">🤔</span>
-        </h1>
+        The Weather Be Like...
+        <span role="img" aria-label="Thinking Face">
+          🤔
+        </span>
+      </h1>
     </div>
   );
 }
- 
+
+export default App;
